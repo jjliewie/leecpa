ea98466dbe96f71585b9b2a82c08d23342c34d65
jjliewie <52589420+jjliewie@users.noreply.github.com>
2024-05-02 21:49:14 -0400

    Update auto.py
    i added naming, priority. :-) I'm practically doing overtime.

--- auto.py
+++ auto.py
@@ -8,6 +8,39 @@ valleywood = 'C:/Users/Julie/Desktop/hv/valleywood.xlsx'
 hbook = load_workbook(filename=humber)
 vbook = load_workbook(filename=valleywood)
 
+months = ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"]
+
+def priority(wb):
+    month = 12
+    day = 31
+    for i in range(len(months)):
+        if months[i] in wb:
+            month = i+1
+            break
+    for i in range(len(wb)):
+        if wb[i].isdigit():
+            if i < len(wb) - 1 and wb[i+1].isdigit():
+                day = int(wb[i] + wb[i+1])
+                break
+            day = int(wb[i])
+            break
+    return month + (day / 100)
+
+def name(wb):
+    start = 0
+    for i in range(len(wb) - 3):
+        k = wb[i] + wb[i+1] + wb[i+2]
+        if k in months:
+            start = i
+            break
+    return wb[i:]
+
+def ordering(lst):
+    pdict = {}
+    for i in lst:
+        pdict[priority(i)] = i
+    return list(dict(sorted(pdict.items())).values())
+
 def copysheet(book, name):
     hmain = book.worksheets[0]
     vmain = book.worksheets[1]
@@ -26,21 +59,10 @@ def copysheet(book, name):
     vbook.save(valleywood)
 
 path = 'C:/Users/Julie/Desktop/hv/all'
-spreadsheets = os.listdir(path)
+spreadsheets = ordering(os.listdir(path))
 # print(spreadsheets)
 
 for wb in spreadsheets:
     specific = path + '/' + wb
     book = load_workbook(filename=specific)
-    copysheet(book, wb)
-
-
-# for file in spreadsheets:
-#     file_name = f"{file}"
-#     wb = load_workbook(filename=file_name)
-#     copysheet(wb)
-
-# for spreadsheet in spreadsheets:
-#     copysheet(spreadsheet)
-
-#print("hello world")
+    copysheet(book, name(wb))
